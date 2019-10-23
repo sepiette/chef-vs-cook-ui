0590ed03f733951b354f191cc92e26ebb65559ab
Sara Piette <sarapiette@Saras-MacBook-Pro.local>
2019-10-23 16:40:27 -0400

    add routing and recipes page component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Route, Link, Switch } from 'react-router-dom';
 import './App.css';
+import Recipes from './components/Recipes';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+      <h1 className="display-4">Food Recipes</h1>
+      <p className="lead">
+        A curated list of recipes for the best homemade meal and delicacies.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <hr className="my-4" />
+      <nav>
+        <Link to="/recipes">Recipes Are Here</Link>
+      </nav>
+      <Switch>
+        <Route path="/recipes" component={Recipes} />
+      </Switch>
     </div>
+
+
+
   );
 }
 
---
+++ src/components/Recipes.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class Recipes extends React.Component {
+  render() {
+    return (<p>Welcome to Recipes</p>);
+  }
+}
+
+export default Recipes;
